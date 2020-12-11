7c51de71188585756c7d50cd07cf06651535a8d5
Pelumi Longe <longe.pelumi@yahoo.com>
2020-12-11 12:12:00 +0100

    debug

--- app/jobs/update_bit_coin_price.rb
+++ app/jobs/update_bit_coin_price.rb
@@ -3,7 +3,9 @@ class UpdateBitCoinPrice
   sidekiq_options queue: :default, retry: false
 
   def perform
+    puts "====="
     puts BitCoin.create!(BitCoinService.bit_coin_detail).inspect
+    puts "====="
 
     ActionCable.server.broadcast(
       'bit_coin_channel', {
